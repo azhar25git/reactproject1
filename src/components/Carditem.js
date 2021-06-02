920558af72b286291e3fa1f7b052ef933763d6a6
azhar <azhar.cu25@gmail.com>
2021-06-02 23:07:22 +0600

    added cards and 1 carditem

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Carditem() {
+function Carditem(props) {
     return (
-        <>
+        <React.Fragment>
             <li className='cards__item'>
-                <Link className='cards__item__link'>
-                    <figure className='cards__item__pic-wrap'>
-                        <img src='/' alt='Travel Image' className='cards__item__image' />
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img src={props.src} alt='Travel Image' className='cards__item__img' />
                     </figure>
                     <div className='cards__item__info'>
-                        <h5 className='cards__item__text'></h5>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
             </li>  
-        </>
+        </React.Fragment>
     )
 }
 
-export default Carditem
+export default Carditem;
